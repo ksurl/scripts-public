c6690ef1d3ce431e613a355a58b265e2f898cfa5
ksurl <ksurl@users.noreply.github.com>
2020-06-15 14:30:01 -0700

    update path.

--- powershell/convert-ebooks.ps1
+++ powershell/convert-ebooks.ps1
@@ -1,24 +1,16 @@
 # uses convert-ebook.exe from calibre to convert all specified ebooks to selected format
 # example: .\convert-ebooks.ps1 -src "c:\books" -inputformat "epub" -outputformat "mobi"
 
-param([string]$src=$PSScriptRoot,[string]$inputformat="epub",[string]$outputformat="mobi")
+param([string]$src="$env:userprofile\desktop\calibre",[string]$inputformat="epub",[string]$outputformat="mobi")
 
-<<<<<<< HEAD
-$books = Get-ChildItem -Path $src -Include "*.$inputformat"
+$books = Get-ChildItem -Path $src -File "*$inputformat"
 Set-Location $src
-foreach ($book in $books) {
-    $dst = $book.fullname.replace(".$inputformat",".$outputformat")
-    if (!(Test-Path $dst)) {
-        Write-Host "Converting $($book.name) to $($book.basename).$outputformat"
-        ebook-convert.exe $book.name $dst
-=======
-$books = Get-ChildItem -Path $src -Include "*.$inputformat" -Recurse
+$books
 
 foreach ($book in $books) {
     $dst = "$($book.directory)\$($book.basename).$outputformat"
     if (!(Test-Path $dst)) {
         Write-Host "Converting $($book.name) to $($book.basename).$outputformat"
         ebook-convert.exe $book.fullname $dst
->>>>>>> 249e847b033d09c67a9681131868e64c1a952040
     }
 }
--- powershell/convert-ebooksrsjob.ps1
+++ powershell/convert-ebooksrsjob.ps1
@@ -1,12 +1,12 @@
 # uses convert-ebook.exe from calibre to convert all specified ebooks to selected format
 # example: .\convert-ebooks.ps1 -src "c:\books" -inputformat "epub" -outputformat "mobi"
 
-param([string]$src=$PSScriptRoot,[string]$inputformat="epub",[string]$outputformat="mobi")
+param([string]$src="$env:userprofile\desktop\calibre",[string]$inputformat="epub",[string]$outputformat="mobi")
 
-$books = Get-ChildItem -Path $src -Include "*.$inputformat"
+$books = Get-ChildItem -Path $src -File "*.$inputformat"
 
- $books | Start-RSJob -Name { $_.name } -Throttle 8 -ScriptBlock {
-    $dst = $book.fullname.trimend($inputformat) + ".$outputformat"
+$books | Start-RSJob -Name { $_.name } -Throttle 8 -ScriptBlock {
+    $dst = $book.fullname.trimend($inputformat) + $outputformat
     if (!(Test-Path $dst)) {
         Write-Host "Converting $($book.name) to $($book.basename).$outputformat"
         ebook-convert.exe $book.fullname $dst
