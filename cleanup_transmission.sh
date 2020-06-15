8a7cf2130d85d6804fb8ff5739bf969e054d2d02
ksurl <ksurl@users.noreply.github.com>
2020-06-15 14:31:20 -0700

    add docker version.

---
+++ cleanup_transmission.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+bin='/usr/bin/transmission-remote'
+
+# use if you have authentication enabled
+#bin='/usr/bin/trasmission-remote  --auth user:password'
+
+for item in `$bin -l | grep 100%.*Done`; do
+    ID=`echo $item | sed "s/^ *//g" | sed "s/ *100%.*//g"`
+    $bin -t $ID -r
+done
